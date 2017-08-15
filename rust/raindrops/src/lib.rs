684cafac43a3f731f4e25709fd8480841f16da1c
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2017-08-15 16:25:44 +0300

    Reimplement Rust raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,9 +1,11 @@
-pub fn raindrops(num: u32) -> &'static str {
+pub fn raindrops(num: u32) -> String {
+    let mut sound = String::new();
     let factor = |x, y| x % y == 0;
-    match num {
-        num if factor(num, 3) => "Pling",
-        num if factor(num, 5) => "Plang",
-        num if factor(num, 7) => "Plong",
-        _ => num.to_string().as_str()
+    if factor(num, 3) { sound.push_str("Pling") };
+    if factor(num, 5) { sound.push_str("Plang") };
+    if factor(num, 7) { sound.push_str("Plong") };
+    if sound.is_empty() {
+        sound = num.to_string()
     }
+    sound
 }
